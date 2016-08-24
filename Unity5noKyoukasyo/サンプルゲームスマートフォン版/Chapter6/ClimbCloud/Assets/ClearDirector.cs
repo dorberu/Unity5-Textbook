58651b8520fad0122795498db3b840f3de2f2cf0
dorberu <dorberu@gmail.com>
2016-08-24 18:54:07 +0900

    教材を追加

---
+++ Unity5noKyoukasyo/サンプルゲームスマートフォン版/Chapter6/ClimbCloud/Assets/ClearDirector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; // LoadSceneを使うために必要!!
+
+public class ClearDirector : MonoBehaviour {
+	void Update() {
+		if(Input.GetMouseButtonDown(0)) {
+			SceneManager.LoadScene("GameScene");
+		}
+	}
+}
