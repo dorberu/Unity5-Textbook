15579a00147152e16ae3471eacf95e0092c4517d
dorberu <dorberu@gmail.com>
2016-08-27 20:27:57 +0900

    CatEscapeを実装

---
+++ CatEscape/Assets/GameDirector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameDirector : MonoBehaviour {
+
+	GameObject hpGauge;
+
+	// Use this for initialization
+	void Start () {
+		this.hpGauge = GameObject.Find ("hpGauge");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DecreaseHp() {
+		this.hpGauge.GetComponent<Image> ().fillAmount -= 0.1f; 
+	}
+}
---
+++ CatEscape/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LButtonDown () {
+		transform.Translate (-3, 0, 0);
+	}
+
+	public void RButtonDown () {
+		transform.Translate (3, 0, 0);
+	}
+}
