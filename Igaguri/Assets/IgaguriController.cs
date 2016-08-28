e9c6304e70d3c7073377d472ed929a9d32a330f4
dorberu <dorberu@gmail.com>
2016-08-28 16:48:04 +0900

    Igaguriを実装

---
+++ Igaguri/Assets/IgaguriController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IgaguriController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Shoot (new Vector3(0, 200, 2000));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Shoot (Vector3 dir) {
+		GetComponent<Rigidbody> ().AddForce (dir);
+	}
+
+	void OnCollisionEnter (Collision other) {
+		GetComponent<Rigidbody> ().isKinematic = true;
+		GetComponent<ParticleSystem> ().Play ();
+	}
+}
